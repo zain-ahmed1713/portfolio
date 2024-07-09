290a5e224cf12a2dd01b5b591782d9fd3b06a28a
Zain Ahmed <zainahmed1713@gmail.com>
2024-07-09 23:45:44 +0500

    Designed navbar displaying name

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="font-neueHaas bg-[#353535] p-4 text-center text-[#A3BE8C] text-2xl tracking-wide md:text-3xl lg:text-4xl">
+      Zain Ahmed
+    </nav>
+  );
+};
+export default Navbar;
