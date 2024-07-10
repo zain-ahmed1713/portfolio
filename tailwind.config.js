6317b6ee5ee7aea8b9d444b2b50a487344a27480
Zain Ahmed <zainahmed1713@gmail.com>
2024-07-10 20:55:05 +0500

    Configured Animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        bounce: {
+          '0%, 100%': {
+            transform: 'translateY(-25%)',
+            animationTimingFunction: 'cubic-bezier(0.8,0,1,1)',
+          },
+          '50%': {
+            transform: 'none',
+            animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
+          },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+        bounce: 'bounce 2s infinite',
+      },
+    },
     fontFamily: {
       'neueHaas': 'Neue Haas Grotesk Display Pro'
     },
