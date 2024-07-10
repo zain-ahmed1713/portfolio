35c9968eff855363ec1340a80c1b9f1c87ec011a
Zain Ahmed <zainahmed1713@gmail.com>
2024-07-10 22:41:33 +0500

    Update line height on desktop devices and add github icon in button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
 
   return (
     <section className="h-[90vh] bg-[#353535] flex flex-col justify-center items-center gap-6 p-3 md:p-4">
-      <h2 className="font-neueHaas font-bold text-[#A3BE8C] text-4xl text-center tracking-wide leading-tight md:text-7xl md:leading-snug lg:text-8xl lg:w-2/4 animate-fadeIn">
+      <h2 className="font-neueHaas font-bold text-[#A3BE8C] text-4xl text-center tracking-wide leading-tight md:text-7xl md:leading-snug lg:leading-[1.2] lg:text-8xl lg:w-2/4 animate-fadeIn">
         Highly Motivated Frontend Web Developer
       </h2>
       <div className="button-container animate-bounce">
@@ -15,7 +15,7 @@ const Hero = () => {
           onClick={redirectToGithub}
           className="font-neueHaas tracking-wide bg-[#A3BE8C] text-[#353535] p-2 rounded-3xl hover:opacity-80 md:text-xl md:p-3"
         >
-          Visit My Github
+          Visit My <i className="fa-brands fa-github"></i>
         </button>
       </div>
     </section>
