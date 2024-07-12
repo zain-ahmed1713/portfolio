61d03190f115c20e8531cddf7196cb853fdc5179
Zain Ahmed <zainahmed1713@gmail.com>
2024-07-12 23:42:38 +0500

    Add Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Zain Ahmed | Portfolio",
@@ -13,7 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="relative w-full flex items-center justify-center">
+          <Navbar />
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
