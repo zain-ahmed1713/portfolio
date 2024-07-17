0a1b9715fa9a9ca28b613d2f956ae57a67dfcc7b
Zain Ahmed <zainahmed1713@gmail.com>
2024-07-17 20:42:40 +0500

    Integrate sanity

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,43 +1,26 @@
-"use client";
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
+import { SanityDocument } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { client, sanityFetch } from "@/sanity/client";
 import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 import Link from "next/link";
-import nineyservices from "@/assets/images/nineyservices.png";
-import gemi from "@/assets/images/gemi.png";
-import weatherapp from "@/assets/images/weatherapp.png";
 
-const projects = [
-  {
-    id: 1,
-    title: "NineyServices",
-    description:
-      "Developed a custom web application for a client to automate their digital services business. Utilized React for a dynamic and responsive user interface, and Firebase for a robust backend solution, including firestore database and authentication services",
-    image: nineyservices,
-    repoLink: "",
-    liveLink: "https://nineyservices-c65e9.firebaseapp.com/",
-  },
-  {
-    id: 2,
-    title: "Gemi - AI Chatbot",
-    description:
-      "A conversational chatbot app that utilizes Google Gemini's API to respond to user's prompts. Powered by React and Tailwind CSS on the Frontend and Google Firebase for User Authentication.",
-    image: gemi,
-    repoLink: "https://github.com/zainahmed1713/gemi-chatbot-app",
-    liveLink: "https://gemi-chatbot-app.vercel.app/",
-  },
-  {
-    id: 3,
-    title: "Weather App",
-    description:
-      "Developed a weather forecast application utilizing the WeatherAPI.com API to deliver accurate weather forecasts for up to 2 days. Integrated Browser's Geolocation API to automatically retrieve and use the user's current location for personalized weather updates",
-    image: weatherapp,
-    repoLink: "https://github.com/zainahmed1713/weather-app",
-    liveLink: "https://zainahmed1713.github.io/weather-app/src/",
-  },
-];
+const PROJECTS_QUERY = `*[
+  _type == "projects"]{_id, name, detail, image, repoLink, liveLink}`;
 
-const ProjectsSection = () => {
+const { projectId, dataset } = client.config();
+const urlFor = (source: SanityImageSource) =>
+  projectId && dataset
+    ? imageUrlBuilder({ projectId, dataset }).image(source)
+    : null;
+
+const ProjectsSection = async () => {
+  const projects = await sanityFetch<SanityDocument[]>({
+    query: PROJECTS_QUERY,
+  });
+
   return (
     <div className="bg-[#000000] p-10 md:p-20">
       <h2 className="text-white text-center text-3xl font-bold mb-2 md:text-4xl lg:text-5xl lg:text-left lg:mb-4">
@@ -53,28 +36,32 @@ const ProjectsSection = () => {
       </p>
       <div className="h-fit flex flex-col gap-2  lg:overflow-scroll lg:flex-row">
         {projects.map((project) => (
-          <CardContainer key={project.id} className="">
+          <CardContainer key={project._id} className="">
             <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
               <CardItem
                 translateZ="50"
                 className="text-xl font-bold text-neutral-600 dark:text-white"
               >
-                {project.title}
+                {project.name}
               </CardItem>
               <CardItem
                 as="p"
                 translateZ="60"
                 className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
               >
-                {project.description}
+                {project.detail[0].children[0].text}
               </CardItem>
               <CardItem translateZ="100" className="w-full mt-4">
                 <Image
-                  src={project.image}
+                  src={`${
+                    project.image
+                      ? urlFor(project.image)?.width(1000).height(1000).url()
+                      : "https://via.placeholder.com/1000x1000"
+                  }`}
                   height="1000"
                   width="1000"
                   className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                  alt="thumbnail"
+                  alt={project.name}
                 />
               </CardItem>
               <div className="flex justify-between items-center mt-20">
@@ -82,16 +69,14 @@ const ProjectsSection = () => {
                   translateZ={20}
                   as={Link}
                   href={
-                    project.repoLink.length === 0
+                    project.repoLink === null
                       ? project.liveLink
                       : project.repoLink
                   }
                   target="__blank"
                   className={`px-4 py-2 rounded-xl text-xs font-normal dark:text-white`}
                 >
-                  {project.repoLink.length === 0
-                    ? "Closed Source"
-                    : "Visit Repo"}
+                  {project.repoLink === null ? "Closed Source" : "Visit Repo"}
                 </CardItem>
                 <Link href={project.liveLink}>
                   <CardItem
--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -1,104 +1,56 @@
-"use client";
 import React from "react";
+import { SanityDocument } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { client, sanityFetch } from "@/sanity/client";
 import { StickyScroll } from "./ui/sticky-scroll-reveal";
 import Image from "next/image";
-import reactjs from "@/assets/images/reactjs.png"
-import nextjs from "@/assets/images/nextjs.png"
-import tailwindcss from "@/assets/images/tailwindcss.png"
-import firebase from "@/assets/images/firebase.png"
-import htmlCssJs from "@/assets/images/htmlCssJs.png"
 
-const content = [
-  {
-    title: "React.js",
-    description:
-      "With its component-based architecture, React allows for the creation of reusable UI components, enhancing code maintainability and scalability. Its virtual DOM improves performance, making updates faster and more efficient.",
+const SKILLS_QUERY = `*[
+  _type == "skills"]{_id, name, detail, image}`;
+
+const { projectId, dataset } = client.config();
+const urlFor = (source: SanityImageSource) =>
+  projectId && dataset
+    ? imageUrlBuilder({ projectId, dataset }).image(source)
+    : null;
+
+const SkillsSection = async () => {
+  const skills = await sanityFetch<SanityDocument[]>({ query: SKILLS_QUERY });
+
+  const skillsArray = skills.map((item) => ({
+    title: item.name,
+    description: item.detail[0].children[0].text,
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
-          <Image
-            src={reactjs}
-            width={300}
-            height={300}
-            className="h-full w-full object-cover"
-            alt="React.js"
-          />
-        </div>
-    ),
-  },
-  {
-    title: "Next.js",
-    description:
-      "Next.js enhances performance through optimized bundling and pre-fetching, making it ideal for modern web applications. Its built-in support for SEO, internationalization, and automatic code-splitting ensures that applications are fast, scalable, and optimized for search engines.",
-    content: (
-      <div className="h-full w-full  flex items-center justify-center text-white">
         <Image
-          src={nextjs}
+          src={`${
+            item.image
+              ? urlFor(item.image)?.width(300).height(300).url()
+              : "https://via.placeholder.com/300x300"
+          }`}
           width={300}
           height={300}
           className="h-full w-full object-cover"
-          alt="Next.js"
+          alt={item.name}
         />
       </div>
     ),
-  },
-  {
-    title: "Tailwind CSS",
-    description:
-      "Tailwind enables a highly customizable and consistent design system without writing traditional CSS. Its approach promotes the creation of responsive, modern, and visually appealing user interfaces. Tailwind's extensive configurability and built-in design patterns help streamline development, reduce code redundancy, and maintain a scalable codebase.",
-    content: (
-      <div className="h-full w-full  flex items-center justify-center text-white">
-          <Image
-            src={tailwindcss}
-            width={300}
-            height={300}
-            className="h-full w-full object-cover"
-            alt="linear board demo"
-          />
-        </div>
-    ),
-  },
-  {
-    title: "Firebase",
-    description:
-      "It offers a suite of cloud-based services including real-time databases, authentication, hosting, cloud functions, and analytics. With Firebase, I can effortlessly implement secure user authentication, store and sync data in real-time across all clients, and deploy applications quickly.",
-    content: (
-      <div className="h-full w-full  flex items-center justify-center text-white">
-          <Image
-            src={firebase}
-            width={300}
-            height={300}
-            className="h-full w-full object-cover"
-            alt="linear board demo"
-          />
-        </div>
-    ),
-  },
-  {
-    title: "HTML, CSS, & JavaScript",
-    description:
-      "HTML, CSS, and JavaScript are the core technologies for building web applications. HTML provides the structure, CSS handles the styling and layout, and JavaScript brings interactivity and dynamic functionality, allowing me to create responsive, visually appealing, and engaging user experiences.",
-    content: (
-        <div className="h-full w-full  flex items-center justify-center text-white">
-          <Image
-            src={htmlCssJs}
-            width={300}
-            height={300}
-            className="h-full w-full object-cover"
-            alt="linear board demo"
-          />
-        </div>
-    ),
-  },
-];
+  }));
 
-const SkillsSection = () => {
   return (
     <div className="bg-[#000000] p-10 md:p-20">
-        <h2 className="text-white text-center text-3xl font-bold mb-2 md:text-4xl lg:text-5xl lg:text-left lg:mb-4">Skills</h2>
-        <p className="text-white text-center text-sm mb-6 md:text-lg lg:w-1/2 lg:text-left">I leverage <strong>Modern Development</strong> practices with a suite of tools and technologies including <strong>React.js</strong>, <strong>Next.js</strong>, and <strong>Tailwind CSS</strong>.</p>
-      <StickyScroll content={content} />
+      <h2 className="text-white text-center text-3xl font-bold mb-2 md:text-4xl lg:text-5xl lg:text-left lg:mb-4">
+        Skills
+      </h2>
+      <p className="text-white text-center text-sm mb-6 md:text-lg lg:w-1/2 lg:text-left">
+        I leverage <strong>Modern Development</strong> practices with a suite of
+        tools and technologies including <strong>React.js</strong>,{" "}
+        <strong>Next.js</strong>, and <strong>Tailwind CSS</strong>.
+      </p>
+      <StickyScroll content={skillsArray} />
     </div>
   );
-}
+};
 
 export default SkillsSection;
